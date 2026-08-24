2cce7d0902a2457d462dc8fdc76f0749ae5ca365
payal2403 <payalc2403@gmail.com>
2026-08-24 16:21:34 +0530

    push

--- frontend/src/layout/Ownerlayout/OwnerFooter.jsx
+++ frontend/src/layout/Ownerlayout/OwnerFooter.jsx
@@ -2,7 +2,6 @@
     return(
         <>
         {/* Footer Start */}
-<<<<<<< HEAD
   <div className="container-fluid footer py-4">
   <div className="container">
 
@@ -37,179 +36,6 @@
               <a href="#" className="btn btn-sm btn-primary rounded-circle">
                 <i className="fab fa-linkedin-in"></i>
               </a>
-=======
- <div className="container-fluid footer py-5" data-aos="fade-up" data-aos-duration="800">
-  <div className="container py-5">
-    <div className="row g-5">
-      <div className="col-xl-9">
-        <div className="mb-5">
-          <div className="row g-4">
-            <div className="col-md-6 col-lg-6 col-xl-5">
-              <div className="footer-item">
-                <a href="index.html" className="p-0">
-                  <h3 className="text-white">
-                    <i className="fab fa-slack me-3" /> LifeSure
-                  </h3>
-                  {/* <img src="img/logo.png" alt="Logo"> */}
-                </a>
-                <p className="text-white mb-4">
-                  Dolor amet sit justo amet elitr clita ipsum elitr est.Lorem
-                  ipsum dolor sit amet, consectetur adipiscing...
-                </p>
-                <div className="footer-btn d-flex">
-                  <a className="btn btn-md-square rounded-circle me-3" href="#">
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                  <a className="btn btn-md-square rounded-circle me-3" href="#">
-                    <i className="fab fa-twitter" />
-                  </a>
-                  <a className="btn btn-md-square rounded-circle me-3" href="#">
-                    <i className="fab fa-instagram" />
-                  </a>
-                  <a className="btn btn-md-square rounded-circle me-0" href="#">
-                    <i className="fab fa-linkedin-in" />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-6 col-xl-3">
-              <div className="footer-item">
-                <h4 className="text-white mb-4">Useful Links</h4>
-                <a href="#">
-                  <i className="fas fa-angle-right me-2" /> About Us
-                </a>
-                <a href="#">
-                  <i className="fas fa-angle-right me-2" /> Features
-                </a>
-                <a href="#">
-                  <i className="fas fa-angle-right me-2" /> Services
-                </a>
-                <a href="#">
-                  <i className="fas fa-angle-right me-2" /> FAQ's
-                </a>
-                <a href="#">
-                  <i className="fas fa-angle-right me-2" /> Blogs
-                </a>
-                <a href="#">
-                  <i className="fas fa-angle-right me-2" /> Contact
-                </a>
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-6 col-xl-4">
-              <div className="footer-item">
-                <h4 className="mb-4 text-white">Instagram</h4>
-                <div className="row g-3">
-                  <div className="col-4">
-                    <div className="footer-instagram rounded">
-                      <img
-                        src="img/instagram-footer-1.jpg"
-                        className="img-fluid w-100"
-                        alt=""
-                      />
-                      <div className="footer-search-icon">
-                        <a
-                          href="img/instagram-footer-1.jpg"
-                          data-lightbox="footerInstagram-1"
-                          className="my-auto"
-                        >
-                          <i className="fas fa-link text-white" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-4">
-                    <div className="footer-instagram rounded">
-                      <img
-                        src="img/instagram-footer-2.jpg"
-                        className="img-fluid w-100"
-                        alt=""
-                      />
-                      <div className="footer-search-icon">
-                        <a
-                          href="img/instagram-footer-2.jpg"
-                          data-lightbox="footerInstagram-2"
-                          className="my-auto"
-                        >
-                          <i className="fas fa-link text-white" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-4">
-                    <div className="footer-instagram rounded">
-                      <img
-                        src="img/instagram-footer-3.jpg"
-                        className="img-fluid w-100"
-                        alt=""
-                      />
-                      <div className="footer-search-icon">
-                        <a
-                          href="img/instagram-footer-3.jpg"
-                          data-lightbox="footerInstagram-3"
-                          className="my-auto"
-                        >
-                          <i className="fas fa-link text-white" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-4">
-                    <div className="footer-instagram rounded">
-                      <img
-                        src="img/instagram-footer-4.jpg"
-                        className="img-fluid w-100"
-                        alt=""
-                      />
-                      <div className="footer-search-icon">
-                        <a
-                          href="img/instagram-footer-4.jpg"
-                          data-lightbox="footerInstagram-4"
-                          className="my-auto"
-                        >
-                          <i className="fas fa-link text-white" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-4">
-                    <div className="footer-instagram rounded">
-                      <img
-                        src="img/instagram-footer-5.jpg"
-                        className="img-fluid w-100"
-                        alt=""
-                      />
-                      <div className="footer-search-icon">
-                        <a
-                          href="img/instagram-footer-5.jpg"
-                          data-lightbox="footerInstagram-5"
-                          className="my-auto"
-                        >
-                          <i className="fas fa-link text-white" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-4">
-                    <div className="footer-instagram rounded">
-                      <img
-                        src="img/instagram-footer-6.jpg"
-                        className="img-fluid w-100"
-                        alt=""
-                      />
-                      <div className="footer-search-icon">
-                        <a
-                          href="img/instagram-footer-6.jpg"
-                          data-lightbox="footerInstagram-6"
-                          className="my-auto"
-                        >
-                          <i className="fas fa-link text-white" />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
->>>>>>> a153fa30a0c87e8795adf128a80e5abb362cc409
             </div>
           </div>
 
@@ -293,41 +119,12 @@
               Subscribe
             </button>
           </div>
-<<<<<<< HEAD
 
           <p className="text-white-50 small mb-0">
             <i className="fas fa-envelope text-primary me-2"></i>
             support@parkease.com
           </p>
 
-=======
-          <div className="d-flex flex-shrink-0">
-            <div className="footer-btn">
-              <a
-                href="#"
-                className="btn btn-lg-square rounded-circle position-relative"
-                data-aos="zoom-in"
-                data-aos-delay="300"
-              >
-                <i className="fa fa-phone-alt fa-2x" />
-                <div
-                  className="position-absolute"
-                  style={{ top: 2, right: 12 }}
-                >
-                  <span>
-                    <i className="fa fa-comment-dots text-secondary" />
-                  </span>
-                </div>
-              </a>
-            </div>
-            <div className="d-flex flex-column ms-3 flex-shrink-0">
-              <span>Call to Our Experts</span>
-              <a href="tel:+ 0123 456 7890">
-                <span className="text-white">Free: + 0123 456 7890</span>
-              </a>
-            </div>
-          </div>
->>>>>>> a153fa30a0c87e8795adf128a80e5abb362cc409
         </div>
       </div>
 
