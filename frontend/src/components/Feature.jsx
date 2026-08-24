9aed4c9c3f38219db858b649320db0dba8dece9b
payal2403 <payalc2403@gmail.com>
2026-08-24 15:33:40 +0530

    push

--- frontend/src/components/Feature.jsx
+++ frontend/src/components/Feature.jsx
@@ -74,100 +74,149 @@ const Feature=()=>{
 
  
   {/* Feature Start */}
-  <div className="container-fluid feature bg-light py-5">
-    <div className="container py-5">
+ <div className="container-fluid feature bg-light py-5">
+  <div className="container py-5">
+
+    {/* Heading */}
+    <div
+      className="text-center mx-auto pb-5 wow fadeInUp"
+      data-wow-delay="0.2s"
+      style={{ maxWidth: 800 }}
+    >
+      <h4 className="text-primary">Our Features</h4>
+
+      <h1 className="display-4 mb-4">
+        Smart Features For Easy Parking
+      </h1>
+
+      <p className="mb-0">
+        Our smart parking system makes parking simple by helping users find,
+        book, and manage parking spaces easily through one convenient platform.
+      </p>
+    </div>
+
+    {/* Features */}
+    <div className="row g-4">
+
+      {/* Feature 1 */}
       <div
-        className="text-center mx-auto pb-5 wow fadeInUp"
+        className="col-6 col-lg-3 wow fadeInUp"
         data-wow-delay="0.2s"
-        style={{ maxWidth: 800 }}
       >
-        <h4 className="text-primary">Our Features</h4>
-        <h1 className="display-4 mb-4">
-          Insurance Provide you a Better Future
-        </h1>
-        <p className="mb-0">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
-          adipisci facilis cupiditate recusandae aperiam temporibus corporis
-          itaque quis facere, numquam, ad culpa deserunt sint dolorem autem
-          obcaecati, ipsam mollitia hic.
-        </p>
-      </div>
-      <div className="row g-4">
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.2s"
-        >
-          <div className="feature-item p-4 pt-0">
-            <div className="feature-icon p-4 mb-4">
-              <i className="far fa-handshake fa-3x" />
-            </div>
-            <h4 className="mb-4">Trusted Company</h4>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic
-              laborum odit pariatur...
-            </p>
-            <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-              Learn More
-            </a>
+        <div className="feature-item p-4 pt-0">
+
+          <div className="feature-icon p-4 mb-4">
+            <i className="far fa-handshake fa-3x" />
           </div>
-        </div>
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.4s"
-        >
-          <div className="feature-item p-4 pt-0">
-            <div className="feature-icon p-4 mb-4">
-              <i className="fa fa-dollar-sign fa-3x" />
-            </div>
-            <h4 className="mb-4">Anytime Money Back</h4>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic
-              laborum odit pariatur...
-            </p>
-            <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-              Learn More
-            </a>
-          </div>
-        </div>
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.6s"
-        >
-          <div className="feature-item p-4 pt-0">
-            <div className="feature-icon p-4 mb-4">
-              <i className="fa fa-bullseye fa-3x" />
-            </div>
-            <h4 className="mb-4">Flexible Plans</h4>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic
-              laborum odit pariatur...
-            </p>
-            <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-              Learn More
-            </a>
-          </div>
-        </div>
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.8s"
-        >
-          <div className="feature-item p-4 pt-0">
-            <div className="feature-icon p-4 mb-4">
-              <i className="fa fa-headphones fa-3x" />
-            </div>
-            <h4 className="mb-4">24/7 Fast Support</h4>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea hic
-              laborum odit pariatur...
-            </p>
-            <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-              Learn More
-            </a>
-          </div>
+
+          <h4 className="mb-4">Easy Parking</h4>
+
+          <p className="mb-4">
+            Find available parking spaces quickly and avoid the hassle of
+            searching for a spot in busy areas.
+          </p>
+
+          <a
+            className="btn btn-primary rounded-pill py-2 px-4"
+            href="#"
+          >
+            Learn More
+          </a>
+
         </div>
       </div>
+
+
+      {/* Feature 2 */}
+      <div
+        className="col-6 col-lg-3 wow fadeInUp"
+        data-wow-delay="0.4s"
+      >
+        <div className="feature-item p-4 pt-0">
+
+          <div className="feature-icon p-4 mb-4">
+            <i className="fa fa-dollar-sign fa-3x" />
+          </div>
+
+          <h4 className="mb-4">Secure Booking</h4>
+
+          <p className="mb-4">
+            Book your parking slot in advance and get a convenient and secure
+            parking experience.
+          </p>
+
+          <a
+            className="btn btn-primary rounded-pill py-2 px-4"
+            href="#"
+          >
+            Learn More
+          </a>
+
+        </div>
+      </div>
+
+
+      {/* Feature 3 */}
+      <div
+        className="col-6 col-lg-3 wow fadeInUp"
+        data-wow-delay="0.6s"
+      >
+        <div className="feature-item p-4 pt-0">
+
+          <div className="feature-icon p-4 mb-4">
+            <i className="fa fa-bullseye fa-3x" />
+          </div>
+
+          <h4 className="mb-4">Smart Management</h4>
+
+          <p className="mb-4">
+            Manage parking slots, vehicles, bookings, and records easily
+            through one smart system.
+          </p>
+
+          <a
+            className="btn btn-primary rounded-pill py-2 px-4"
+            href="#"
+          >
+            Learn More
+          </a>
+
+        </div>
+      </div>
+
+
+      {/* Feature 4 */}
+      <div
+        className="col-6 col-lg-3 wow fadeInUp"
+        data-wow-delay="0.8s"
+      >
+        <div className="feature-item p-4 pt-0">
+
+          <div className="feature-icon p-4 mb-4">
+            <i className="fa fa-headphones fa-3x" />
+          </div>
+
+          <h4 className="mb-4">24/7 Support</h4>
+
+          <p className="mb-4">
+            Get quick assistance whenever you need help with parking,
+            bookings, or your account.
+          </p>
+
+          <a
+            className="btn btn-primary rounded-pill py-2 px-4"
+            href="#"
+          >
+            Learn More
+          </a>
+
+        </div>
+      </div>
+
     </div>
   </div>
+</div>
+
   {/* Feature End */}
   <p />
 
--- frontend/src/components/Service.jsx
+++ frontend/src/components/Service.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Service=()=>{
     return(
 
@@ -18,11 +19,11 @@ const Service=()=>{
         data-wow-delay="0.3s"
       >
         <li className="breadcrumb-item">
-          <a href="index.html">Home</a>
+          <Link to="/">Home</Link>
         </li>
-        <li className="breadcrumb-item">
-          <a href="#">Solutions</a>
-        </li>
+        {/* <li className="breadcrumb-item"> */}
+          {/* <a href="#">Solutions</a> */}
+        {/* </li> */}
         <li className="breadcrumb-item active text-primary">
           Parking Services
         </li>
@@ -78,157 +79,229 @@ const Service=()=>{
 
 
   {/* Service Start */}
-  <div className="container-fluid service py-5">
-    <div className="container py-5">
+<div className="container-fluid service py-5">
+  <div className="container py-5">
+
+    {/* Heading */}
+    <div
+      className="text-center mx-auto pb-5 wow fadeInUp"
+      data-wow-delay="0.2s"
+      style={{ maxWidth: 800 }}
+    >
+      <h4 className="text-primary">Our Parking Services</h4>
+
+      <h1 className="display-4 mb-4">
+        Smart Parking Solutions For Modern Cities
+      </h1>
+
+      <p className="mb-0">
+        Our smart parking management system helps users easily find available
+        parking spaces, book slots online, manage vehicles efficiently, and
+        reduce traffic congestion using real-time parking technology and
+        automated monitoring.
+      </p>
+    </div>
+
+    {/* Cards */}
+    <div className="row g-4">
+
+      {/* Card 1 */}
       <div
-        className="text-center mx-auto pb-5 wow fadeInUp"
+        className="col-sm-6 col-lg-3 wow fadeInUp"
         data-wow-delay="0.2s"
-        style={{ maxWidth: 800 }}
       >
-        <h4 className="text-primary">Our Parking Services</h4>
-        <h1 className="display-4 mb-4">
-          Smart Parking Solutions For Modern Cities
-        </h1>
-        <p className="mb-0">
-          Our smart parking management system helps users easily find available
-          parking spaces, book slots online, manage vehicles efficiently, and
-          reduce traffic congestion using real-time parking technology and
-          automated monitoring.
-        </p>
-      </div>
-      <div className="row g-4 justify-content-center">
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.2s"
-        >
-          <div className="service-item">
-            <div className="service-img">
-              <img
-                src="https://images.unsplash.com/photo-1506521781263-d8422e82f27a"
-                className="img-fluid rounded-top w-100"
-                alt=""
-              />
-              <div className="service-icon p-3">
-                <i className="fa fa-map-marker-alt fa-2x" />
-              </div>
-            </div>
-            <div className="service-content p-4">
-              <div className="service-content-inner">
-                <a href="#" className="d-inline-block h4 mb-4">
-                  Real-Time Slot Tracking
-                </a>
-                <p className="mb-4">
-                  Users can check live parking availability and find the nearest
-                  free parking space instantly.
-                </p>
-                <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-                  Read More
-                </a>
-              </div>
+        <div className="service-item">
+
+          <div className="service-img">
+            <img
+              src="https://images.unsplash.com/photo-1506521781263-d8422e82f27a"
+              className="img-fluid rounded-top w-100"
+              alt="Real-Time Slot Tracking"
+            />
+
+            <div className="service-icon p-3">
+              <i className="fa fa-map-marker-alt fa-2x" />
             </div>
           </div>
-        </div>
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.4s"
-        >
-          <div className="service-item">
-            <div className="service-img">
-              <img
-                src="https://images.unsplash.com/photo-1503376780353-7e6692767b70"
-                className="img-fluid rounded-top w-100"
-                alt=""
-              />
-              <div className="service-icon p-3">
-                <i className="fa fa-calendar-check fa-2x" />
-              </div>
-            </div>
-            <div className="service-content p-4">
-              <div className="service-content-inner">
-                <a href="#" className="d-inline-block h4 mb-4">
-                  Online Parking Booking
-                </a>
-                <p className="mb-4">
-                  Reserve parking slots in advance through a smart digital
-                  booking system for convenience.
-                </p>
-                <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-                  Read More
-                </a>
-              </div>
+
+          <div className="service-content p-4">
+            <div className="service-content-inner">
+
+              <a href="#" className="d-inline-block h4 mb-4">
+                Real-Time Slot Tracking
+              </a>
+
+              <p className="mb-4">
+                Users can check live parking availability and find the nearest
+                free parking space instantly.
+              </p>
+
+              <a
+                className="btn btn-primary rounded-pill py-2 px-4"
+                href="#"
+              >
+                Read More
+              </a>
+
             </div>
           </div>
-        </div>
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.6s"
-        >
-          <div className="service-item">
-            <div className="service-img">
-              <img
-                src="https://images.unsplash.com/photo-1502877338535-766e1452684a"
-                className="img-fluid rounded-top w-100"
-                alt=""
-              />
-              <div className="service-icon p-3">
-                <i className="fa fa-car fa-2x" />
-              </div>
-            </div>
-            <div className="service-content p-4">
-              <div className="service-content-inner">
-                <a href="#" className="d-inline-block h4 mb-4">
-                  Vehicle Management
-                </a>
-                <p className="mb-4">
-                  Manage multiple vehicles, entry records, and parking history
-                  through an automated system.
-                </p>
-                <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp"
-          data-wow-delay="0.8s"
-        >
-          <div className="service-item">
-            <div className="service-img">
-              <img
-                src="https://images.unsplash.com/photo-1549924231-f129b911e442"
-                className="img-fluid rounded-top w-100"
-                alt=""
-              />
-              <div className="service-icon p-3">
-                <i className="fa fa-chart-line fa-2x" />
-              </div>
-            </div>
-            <div className="service-content p-4">
-              <div className="service-content-inner">
-                <a href="#" className="d-inline-block h4 mb-4">
-                  Admin Monitoring Dashboard
-                </a>
-                <p className="mb-4">
-                  Admins can track parking usage, bookings, revenue, and slot
-                  analytics in real time.
-                </p>
-                <a className="btn btn-primary rounded-pill py-2 px-4" href="#">
-                  Read More
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 text-center wow fadeInUp" data-wow-delay="0.2s">
-          <a className="btn btn-primary rounded-pill py-3 px-5" href="#">
-            Explore All Features
-          </a>
+
         </div>
       </div>
+
+
+      {/* Card 2 */}
+      <div
+        className="col-sm-6 col-lg-3 wow fadeInUp"
+        data-wow-delay="0.4s"
+      >
+        <div className="service-item">
+
+          <div className="service-img">
+            <img
+              src="https://images.unsplash.com/photo-1503376780353-7e6692767b70"
+              className="img-fluid rounded-top w-100"
+              alt="Online Parking Booking"
+            />
+
+            <div className="service-icon p-3">
+              <i className="fa fa-calendar-check fa-2x" />
+            </div>
+          </div>
+
+          <div className="service-content p-4">
+            <div className="service-content-inner">
+
+              <a href="#" className="d-inline-block h4 mb-4">
+                Online Parking Booking
+              </a>
+
+              <p className="mb-4">
+                Reserve parking slots in advance through a smart digital
+                booking system for convenience.
+              </p>
+
+              <a
+                className="btn btn-primary rounded-pill py-2 px-4"
+                href="#"
+              >
+                Read More
+              </a>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+      {/* Card 3 */}
+      <div
+        className="col-sm-6 col-lg-3 wow fadeInUp"
+        data-wow-delay="0.6s"
+      >
+        <div className="service-item">
+
+          <div className="service-img">
+            <img
+              src="https://images.unsplash.com/photo-1502877338535-766e1452684a"
+              className="img-fluid rounded-top w-100"
+              alt="Vehicle Management"
+            />
+
+            <div className="service-icon p-3">
+              <i className="fa fa-car fa-2x" />
+            </div>
+          </div>
+
+          <div className="service-content p-4">
+            <div className="service-content-inner">
+
+              <a href="#" className="d-inline-block h4 mb-4">
+                Vehicle Management
+              </a>
+
+              <p className="mb-4">
+                Manage multiple vehicles, entry records, and parking history
+                through an automated system.
+              </p>
+
+              <a
+                className="btn btn-primary rounded-pill py-2 px-4"
+                href="#"
+              >
+                Read More
+              </a>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+      {/* Card 4 */}
+      <div
+        className="col-sm-6 col-lg-3 wow fadeInUp"
+        data-wow-delay="0.8s"
+      >
+        <div className="service-item">
+
+          <div className="service-img">
+            <img
+              src="https://images.unsplash.com/photo-1549924231-f129b911e442"
+              className="img-fluid rounded-top w-100"
+              alt="Admin Monitoring Dashboard"
+            />
+
+            <div className="service-icon p-3">
+              <i className="fa fa-chart-line fa-2x" />
+            </div>
+          </div>
+
+          <div className="service-content p-4">
+            <div className="service-content-inner">
+
+              <a href="#" className="d-inline-block h4 mb-4">
+                Admin Monitoring Dashboard
+              </a>
+
+              <p className="mb-4">
+                Admins can track parking usage, bookings, revenue, and slot
+                analytics in real time.
+              </p>
+
+              <a
+                className="btn btn-primary rounded-pill py-2 px-4"
+                href="#"
+              >
+                Read More
+              </a>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+      {/* Explore Button */}
+      <div
+        className="col-12 text-center wow fadeInUp"
+        data-wow-delay="0.2s"
+      >
+        <Link
+          className="btn btn-primary rounded-pill py-3 px-5"
+          to="/Feature"
+        >
+          Explore All Features
+        </Link>
+      </div>
+
     </div>
   </div>
+</div>
+
   {/* Service End */}
   <p />
 </>
